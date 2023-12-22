2d14ce3305b6df151712c0349cf887bed0a4d444
Vikeesalunkhe <vikeesalunkhe2002@gmail.com>
2023-12-22 22:48:20 +0530

    Decoretor_Chaining

--- Decoretors/Function_Decorators/4_program.py
+++ Decoretors/Function_Decorators/4_program.py
@@ -1,1 +1,37 @@
+##Decoretor Chaining
 
+def decorFun(Func):
+
+    def wrapper():
+        print("Start wrapper2")
+        Func()
+        print("End wrapper2")
+
+    return wrapper
+
+def decorRun(Func):
+
+    def wrapper():
+        print("Start wrapper1")
+        Func()
+        print("End wrapper1")
+
+    return wrapper
+
+@decorFun
+@decorRun
+def normalFun():
+    print("IN normal fun")
+
+
+normalFun()
+
+
+"""
+O/p Start wrapper2
+    Start wrapper1
+    IN normal fun
+    End wrapper1
+    End wrapper2
+
+"""
