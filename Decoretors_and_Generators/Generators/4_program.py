c6c8bf4515e6b2f4d983b6ec305fade156710cac
Vikeesalunkhe <vikeesalunkhe2002@gmail.com>
2023-12-27 22:43:57 +0530

    Generator

---
+++ Decoretors_and_Generators/Generators/4_program.py
@@ -0,0 +1,28 @@
+
+def fun():
+
+    print("Start fun")
+    yield 10+10
+    yield 20+20
+    yield 30+30
+    print("End fun")
+
+
+
+
+ret = fun()
+print(next(ret))
+print(next(ret))
+print(next(ret))
+print(next(ret))                             #for this line 
+
+
+"""
+O/p Start fun
+    20
+    40
+    60
+    Error : StopIteration
+
+"""
+
