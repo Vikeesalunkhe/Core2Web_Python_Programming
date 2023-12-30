7a57cfbb95cf570b547731542b13c8c9d184e263
Vikeesalunkhe <vikeesalunkhe2002@gmail.com>
2023-12-30 23:30:13 +0530

    print_fun_in_class

---
+++ Class_and_Objects/12_program.py
@@ -0,0 +1,22 @@
+class Demo:
+
+    print("Start Class")
+    def __init__(self):
+        print("In Constructor")
+
+    print("End Class")
+
+
+print("Start Code")
+obj = Demo()
+print("End Code")
+
+
+
+"""
+O/p Start Class
+    End Class
+    Start Code
+    In Constructor
+    End Code
+"""
