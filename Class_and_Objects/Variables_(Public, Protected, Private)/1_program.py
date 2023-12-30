789a9319484038c9c75fa237b1e2a7fe2ef3ced1
Vikeesalunkhe <vikeesalunkhe2002@gmail.com>
2023-12-30 23:37:07 +0530

    class_and_Instance_Variable

---
+++ Class_and_Objects/Variables_(Public, Protected, Private)/1_program.py
@@ -0,0 +1,22 @@
+class Data:
+
+    z = 30
+    def __init__(self):
+        self.x = 10
+        self.y = 20
+    
+    def dispData(self):
+        print(self.x)
+        print(self.y)
+        print(self.z)
+
+
+obj = Data()
+obj.dispData()
+
+
+"""
+O/p 10
+    20
+    30
+"""
