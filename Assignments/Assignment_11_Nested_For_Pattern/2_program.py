9d44c3395f9183c8207428c85fd613a22da198ee
Vikeesalunkhe <vikeesalunkhe2002@gmail.com>
2024-01-01 17:54:53 +0530

    Assignment_11_Pattern_for_loop

--- Assignments/Assignment_11_Nested_For_Pattern/2_program.py
+++ Assignments/Assignment_11_Nested_For_Pattern/2_program.py
@@ -7,4 +7,11 @@
 5 4 3 2
 5 4 3 2 1
 """
+num = int(input("Enter Value of Row number : "))
+for i in range(1,num+1):
+    data = num
+    for j in range(i):
+        print(data, end = " ")
+        data-=1
 
+    print()
