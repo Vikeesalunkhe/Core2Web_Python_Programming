b8d11c31833946e748d98d3d5a18c4b64e7016e8
Vikeesalunkhe <vikeesalunkhe2002@gmail.com>
2024-01-02 19:42:45 +0530

    Assignment_10_Pattern

--- Assignments/Assignment_10_Nested_For_Pattern_02/1_program.py
+++ Assignments/Assignment_10_Nested_For_Pattern_02/1_program.py
@@ -5,5 +5,14 @@ Program1 : Rows = 3
 16 25 36
 49 64 81
 """
+rows = int(input("Enter Value of rows number : "))
+num = 1
+add = 3
 
+for i in range(rows):
+    for j in range(rows):
+        print(num, end = "\t")
+        num +=add
+        add+=2 
 
+    print()
