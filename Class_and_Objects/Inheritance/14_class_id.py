0d39897409afaa10e5a9ecf302b4d66988b94cb4
Vikeesalunkhe <vikeesalunkhe2002@gmail.com>
2024-01-07 00:37:21 +0530

    print_type_of_class_name,obj

---
+++ Class_and_Objects/Inheritance/14_class_id.py
@@ -0,0 +1,13 @@
+class Demo:
+
+    def __eq__(self,obj2):
+        return True
+
+obj1 = Demo()
+obj2 = Demo()
+
+print(obj1 == obj2)
+
+
+#O/p True
+
---
+++ Class_and_Objects/Inheritance/15_class_id.py
@@ -0,0 +1,17 @@
+class Demo:
+    pass
+
+obj = Demo()
+
+print(type(Demo))
+print(type(type))
+print(type(obj))
+print(type(object))
+
+
+'''
+O/p <class 'type'>
+    <class 'type'>
+    <class '__main__.Demo'>
+    <class 'type'>
+'''
