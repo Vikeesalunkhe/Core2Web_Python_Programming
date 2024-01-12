9b2136e34f68d6ae1c74ce6fa407d30bf56fe634
Vikeesalunkhe <vikeesalunkhe2002@gmail.com>
2024-01-12 07:00:04 +0530

    Ckeck Sorted Array

---
+++ Collection_of_Python/Array/19_Ckeck_Sorted_Array.py
@@ -0,0 +1,15 @@
+import array
+
+arr = array.array('i',[1,2,3,3,4,5,6])
+ln = len(arr)
+
+
+for i in range(1,ln):
+    if arr[i-1] <= arr[i]:
+        pass
+
+    else:
+        print("False")
+
+
+
---
+++ Collection_of_Python/Array/20_Ckeck_Sorted_Array_2.py
@@ -0,0 +1,17 @@
+import array
+
+def Sorted(ln,arr):
+
+    i = 1
+    while (i<ln):
+        if arr[i] >= arr[i-1]:
+            pass
+
+        else:
+            return False
+        i+=1
+
+arr = array.array('i',[1,3,3,4,6,7,8])
+ln = len(arr)
+print(Sorted(ln,arr))
+
