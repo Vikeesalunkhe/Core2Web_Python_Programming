34a70abe86214a1d9b1a71fc196549e1307b8a07
Vikeesalunkhe <vikeesalunkhe2002@gmail.com>
2024-01-20 23:02:34 +0530

    Abstract method

--- Class_and_Objects/Abstract_Class/1_program.py
+++ Class_and_Objects/Abstract_Class/1_program.py
@@ -1,6 +1,6 @@
-from abc import ABC
+from abc import ABC, abstractmethod
 
-class Demo(ABC):
+class Main(ABC):
 
     def __init__(self):
         print("In constructor")
@@ -9,6 +9,28 @@ class Demo(ABC):
     def AbstractMethod(self):
         print("In Abstract Method")
 
+class SubMain1(Main):
+
+    def AbstractMethod(self):
+        print("Sub main 1")
+
+class SubMain2(Main):
+
+    def AbstractMethod(self):
+        print("Sub main 2")
 
 
+obj1 = SubMain1()
+obj1.AbstractMethod()
+
+obj2 = SubMain2()
+obj2.AbstractMethod()
+
+
+"""
+In constructor
+Sub main 1
+In constructor
+Sub main 2
+"""
 
