341917a438d87e1a65fb3ec51043e509e7ca0036
Vikeesalunkhe <vikeesalunkhe2002@gmail.com>
2024-01-28 19:32:35 +0530

    Exception handling(try,except,else)

--- Exception_Handling/5_program.py
+++ Exception_Handling/5_program.py
@@ -7,21 +7,25 @@ print("Start code")
 try:
     index1 = int(input("Enter value of index1 : "))
     print(list1[index1])
+except(ValueError, TypeError, IndexError) as err:
+    print(err)
+    index1 = int(input("Enter value of index1 : "))
+    print(list1[index1])
+
+try:
 
     index2 = int(input("Enter value of index2 : "))
     print(list1[index2])
+except(ValueError, TypeError, IndexError):
+    index2 = int(input("Enter value of index2 : "))
 
+try:
     add = list1[index1] + list1[index2]
 
+except(ValueError, TypeError, IndexError):
+    print('Handled')
 
-except ValueError:
-    print("valueError Handeld")
 
-except TypeError:
-    print("TypeError Handeld")
-
-except IndexError:
-    print("IndexError Handeld")
 
 
 else:
