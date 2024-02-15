b66a1f3d1ebd86b2c83fde21c5530ad7517640bf
Vikeesalunkhe <vikeesalunkhe2002@gmail.com>
2024-02-16 00:35:24 +0530

    tell_seek

--- File_Handling/Seek_tell_read.py
+++ File_Handling/Seek_tell_read.py
@@ -1,3 +1,23 @@
 Fileobj = open("Language_Data.txt", "r+")
 
 print(Fileobj.read())
+
+Fileobj.seek(10)
+print(Fileobj.read())
+
+
+"""File_Data
+Java
+Cpp
+Python
+Dart
+BackEnd
+FrontEnd
+
+''''O/p
+ython
+Dart
+BackEnd
+FrontEnd
+"""
+
